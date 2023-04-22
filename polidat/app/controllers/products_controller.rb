485e5a6fd4d9072fd99060f4acc1f2b8961047d8
paragoncastile <paragoncastile@gmail.com>
2023-04-22 09:31:30 -0500

    integrate stripe product

--- polidat/app/controllers/products_controller.rb
+++ polidat/app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
+  
   def index
+    @products = current_user.products.order(created_at: :desc)
   end
 
   def show
@@ -8,6 +10,24 @@ class ProductsController < ApplicationController
   def new
   end
 
+  def create
+    @product = current_user.products.new(product_params)
+    if @product.save
+      service = StripeProduct.new(params, @product)
+      service.create_product
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
   def edit
   end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:name, :description)
+    end
+
 end
