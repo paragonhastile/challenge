c20ccdaa4e6180e0e429d7a2e2a21532eeaae376
paragoncastile <paragoncastile@gmail.com>
2023-04-25 19:57:06 -0500

    add express app

---
+++ api-polidat/src/app.ts
@@ -0,0 +1,20 @@
+import 'express-async-errors';
+
+import express, { json } from 'express';
+import helmet from 'helmet';
+
+const app = express();
+app.use(json());
+app.use(helmet());
+
+app.get('/ping', (_, res) => {
+  res.status(200).json({
+    msg: 'pong',
+  });
+});
+
+app.use((_, res, _2) => {
+  res.status(404).json({ error: 'NOT FOUND' });
+});
+
+export { app };
